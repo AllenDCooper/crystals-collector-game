b50b03090513388c56539cdc9300f0179d023367
ADC04 <allendcooper@gmail.com>
2019-05-14 23:45:49 -0400

    added images, outlined javascript functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,23 @@
-<script src="https://code.jquery.com/jquery-3.4.1.min.js">
+//function for generating random number between 19 and 120
 
-</script>
+//function for generating four random values between 1 and 12
+
+//function for setting up a round, that creates object round that stores randomNum, array of jewelVals, curPoints
+
+//function for updating round
+
+//function for determining if user won
+
+//function for determining if user lost
+
+//function for determining if end of round
+
+//function for setting up a game
+
+//function for starting new round
+
+//assigning variable game to game object, to store wins, losses, and round object
+
+//functions for printing game object to document
+
+//event listener for user click
